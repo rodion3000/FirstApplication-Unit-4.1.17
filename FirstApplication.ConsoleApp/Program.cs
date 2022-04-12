fa300f79d5b09536871c1ccc83f9d8f2429affeb
rodion3000 <rodion.papizh@mail.ru>
2022-04-12 12:16:46 +0500

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;//пространство имен подключение
+namespace FirstApplication.ConsoleApp// обьявление пространства имен для данного кода
+{
+
+	class Programm// обьявление класса programm
+	{
+		static void Main(string[] args)//обьявление метода main
+		{
+			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+			var color = Console.ReadLine();
+
+			if (color == "red")
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+			}
+
+			else if (color == "green")
+			{
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan!");
+
+
+			}
+		}
+	}
+}
+
